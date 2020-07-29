ba278ad076d5add74d831fb4ef5f0997bfbb3619
xiepengqi <xiepengqi@huobi.com>
2020-07-29 19:48:45 +0800

    init

---
+++ union-docs-ui/src/components/MenuElem.vue
@@ -0,0 +1,23 @@
+<template>
+  <el-submenu v-if="data.child && data.child.length >0" :index="data.name">
+    <template slot="title">{{ data.name }}</template>
+    <menu-item :data="data.child" />
+  </el-submenu>
+  <el-menu-item v-else :index="data.name">{{data.name}}</el-menu-item>
+</template>
+
+<script>
+export default {
+  props: {
+    // {name: "name", child: []}
+    data: {
+      type: Object,
+      default: () => {}
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
